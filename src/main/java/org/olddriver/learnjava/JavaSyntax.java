7a4e536be08441a78e7e4274a9eb29eac50965b0
OldDriver9527 <OldDriver@LAPTOP-RVI1RVF9>
2019-10-22 20:10:36 +0800

    创建项目

---
+++ src/main/java/org/olddriver/learnjava/JavaSyntax.java
@@ -0,0 +1,17 @@
+package org.olddriver.learnjava;
+
+public class JavaSyntax {
+	/*
+	 * java 
+	 * java既是一种高级编程语言，也是一个平台
+	 * 使用java语言编写代码包含在.java源文件中，通过javac编译器将源文件编译成字节码文件，然后通过java启动工具
+	 * 可以在虚拟机上运行程序
+	 * 平台一般指程序运行的硬件及软件环境，java平台不同于其他大多数平台，仅是软件环境。
+	 * java平台由java虚拟机和api组成
+	 * 
+	 * 关键字 keyword
+	 * java程序中被赋予特殊含义的单词，关键字都小写
+	 * 
+	 * 
+	 */
+}
