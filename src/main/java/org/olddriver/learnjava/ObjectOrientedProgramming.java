5bc7219926f72dd86b239c90fff160b8b2bed266
OldDriver9527 <OldDriver@10.6.163.20>
2019-11-03 13:26:03 +0800

    面向对象

---
+++ src/main/java/org/olddriver/learnjava/ObjectOrientedProgramming.java
@@ -0,0 +1,20 @@
+package org.olddriver.learnjava;
+
+public class ObjectOrientedProgramming {
+	/*
+	 * 对象 Object
+	 * 软件对象和现实中对象对应，对象是一种奇特变量，由属性和行为构成，成员变量存储对象属性，方法暴露对象行为
+	 * 
+	 * 类 	Class
+	 * 类是对具有相同属性和行为的对象的抽象，类是创建对象的蓝图
+	 * 类是java程序的基本单元
+	 * 
+	 * 面向对象思想
+	 * 将现实中对象抽象为类，在java程序中使用类的实例解决问题
+	 * 
+	 * 面向对象程序特点
+	 * java程序是对象的集合
+	 * 
+	 * 声明类
+	 */
+}
