09b5ab2b18220126f01c4873a0d3620df37c636d
OldDriver9527 <1187557502@qq.com>
2019-11-16 17:08:59 +0800

    老司机

--- src/main/java/org/olddriver/learnjava/oo/Classes.java
+++ src/main/java/org/olddriver/learnjava/oo/Classes.java
@@ -5,7 +5,7 @@ public class Classes {
 	 * 声明类
 	 * 声明类会定义一个新引用类型， 声明类分为普通类声明，枚举声明
 	 * i.类声明格式
-	 * modifier class ClassName{
+	 * modifier class ClassName extends SuperClassName implements Interface List{
 	 * 		constructor,field,method
 	 * }
 	 * 类仅有两种访问权限，public及package
@@ -15,7 +15,7 @@ public class Classes {
 	 * i.成员变量随对象的创建而创建，随对象的销毁而销毁
 	 * 	 局部变量随所属区域的执行而创建，随所属区域的结束而消失
 	 * ii.成员变量存储在堆内存中，局部变量存储在栈内存中
-	 * iii.成员变量存在默认初始化值，局部变量不存在默认初始化值
+	 * iii.成员变量存在默认初始化值；局部变量不存在默认初始化值，在使用局部变量之前要确保其已经初始化完毕
 	 * iiii.成员变量定义在类中，局部变量定义在方法中
 	 * 
 	 * 构造器	Constructor
