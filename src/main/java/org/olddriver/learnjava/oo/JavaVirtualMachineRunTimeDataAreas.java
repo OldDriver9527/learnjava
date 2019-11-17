28d6f8da3553b9d5c2f9b6f724ea398b185a467f
OldDriver9527 <1187557502@qq.com>
2019-11-17 19:05:29 +0800

    老司机

--- src/main/java/org/olddriver/learnjava/oo/JavaVirtualMachineRunTimeDataAreas.java
+++ src/main/java/org/olddriver/learnjava/oo/JavaVirtualMachineRunTimeDataAreas.java
@@ -8,6 +8,7 @@ public class JavaVirtualMachineRunTimeDataAreas {
 	 * java虚拟机线程私有，随着虚拟机线程的创建而创建，随着虚拟机线程的销毁而销毁
 	 * iii.堆 Heap 
 	 * java虚拟机线程共享，随着虚拟机的启动而创建，随着虚拟机的停止而消失
+	 * 堆中包含方法区
 	 * 堆中存储对象
 	 * iiii.方法区 Method Area
 	 * java虚拟机线程共享，随着虚拟机的启动而创建，随着虚拟机的停止而消失
