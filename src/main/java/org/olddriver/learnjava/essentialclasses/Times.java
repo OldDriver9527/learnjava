27ccd767c1d0a83ff97a457348e23590a3139367
OldDriver9527 <1187557502@qq.com>
2020-05-29 23:23:50 +0800

    试试

--- src/main/java/org/olddriver/learnjava/essentialclasses/Times.java
+++ src/main/java/org/olddriver/learnjava/essentialclasses/Times.java
@@ -20,7 +20,8 @@ public class Times {
      *
      * 常用类
      * Duration 表示秒或纳秒的时间间隔
-     * Period   表示一段时间
+     * Period   表示一段时间，实现TemporalAmount接口
+     * of(int,int,int)  创建Period对象，封装指定时间段，参数为年月日
      *
      * Instant  表示时间线上一点
      * now()    创建Instant对象，封装当前格林尼治时间
@@ -32,10 +33,26 @@ public class Times {
      * plusWeeks(long)  在当前时间上增加指定周数，返回新LocalDate对象
      * plusMonths(long) 在当前时间上增加指定月数，返回新LocalDate对象
      * plusYears(long)  在当前时间上增加指定年数，返回新LocalDate对象
+     * plus(TemporalAmount) 在当前时间上增加指定时间段，返回新LocalDate对象
+     *                      Period实现TemporalAmount接口
+     * plus(long,TemporalUnit)  当前时间上添加指定时间，通过TemporalUnit控制时间单位
+     *                          ChronoUnit枚举实现TemporalUnit接口
      * minusDays(long)  在当前时间上减少指定天数，返回新LocalDate对象
      * minusWeeks(long) 在当前时间上减少指定周数，返回新LocalDate对象
      * minusMonths(long)    在当前时间上减少指定月数，返回新LocalDate对象
      * minusYears(long) 在当前时间上减少指定年数，返回新LocalDate对象
+     * minus(TemporalAmount)    在当前时间上减少指定时间段，返回新LocalDate对象
+     * minus(long, TemporalUnit)
+     * withMonth(int)   将当前时间中月修改为指定参数
+     * with(TemporalField, long)    对当前时间中时间字段进行修改，通过TemporalField指定时间字段
+     *                              ChronoField枚举实现TemporalField接口
+     * with(TemporalAdjuster)   通过时间调节器，将时间修改为指定参数
+     *                          TemporalAdjusters中提供创建TemporalAdjuster static方法
+     * isAfter(ChronoLocalDate) 判断当前时间是否在指定时间之后
+     * isBefore(ChronoLocalDate)    判断当前时间是否在指定时间之前
+     * isEqual(ChronoLocalDate) 判断当前时间与指定时间是否相等
+     * format(DateTimeFormatter)    将LocalDate格式化为String
+     * parse(CharSequence, DateTimeFormatter)   将字符序列解析为LocalDate
      *
      * LocalTime
      * now()    创建LocalTime对象，封装当前时分秒
@@ -76,5 +93,15 @@ public class Times {
      *
      * Month    月份枚举，传入月份时建议使用月份枚举，不建议使用数字
      * of(int)  根据指定月份获取枚举常量
+     *
+     * TemporalAdjusters    用于创建TemporalAdjuster
+     * next(DayOfWeek)  创建TemporalAdjuster，将时间调节为下周指定日期
+     * previous(DayOfWeek)  创建TemporalAdjuster，将时间调节为上周指定日期
+     *
+     * ChronoUnit
+     * between(Temporal, Temporal)  计算两时间间隔
+     *
+     * DateTimeFormatter
+     * ofPattern(String)    使用指定格式创建格式化器
      */
 }
