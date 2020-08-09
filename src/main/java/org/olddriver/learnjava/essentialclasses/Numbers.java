b145ffacb3694e19686472b25a09b6c11aa75530
OldDriver9527 <1187557502@qq.com>
2020-08-09 17:47:59 +0800

    试试

--- src/main/java/org/olddriver/learnjava/essentialclasses/Numbers.java
+++ src/main/java/org/olddriver/learnjava/essentialclasses/Numbers.java
@@ -7,7 +7,12 @@ package org.olddriver.learnjava.essentialclasses;
 public class Numbers {
     /*
      *
+     * Number
+     * 数字抽象类，是Byte，Short，Integer，Long，Float，Double父类
      *
+     * 数字格式化输出
+     * DecimalFormat 用于格式化十进制数字
      *
+     * 自动装箱，自动拆箱(Autoboxing and Unboxing)
      */
 }
