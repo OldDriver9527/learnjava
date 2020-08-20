eb53f98f049beefd884567e974381e6684f1fbd0
OldDriver9527 <1187557502@qq.com>
2020-08-20 11:10:52 +0800

    试试

--- src/main/java/org/olddriver/learnjava/oo/Encapsulation.java
+++ src/main/java/org/olddriver/learnjava/oo/Encapsulation.java
@@ -7,9 +7,8 @@ package org.olddriver.learnjava.oo;
 public class Encapsulation {
     /*
      * 封装(Encapsulation)
-     * 将字段，方法包裹在类中。
-     * 封装也能理解为数据隐藏，设计类时通常将字段使用private修饰，意味着该字段对其他类隐藏，通过提供的public方法
-     * 维护字段值
+     * 将字段，方法包裹在类中，进行数据隐藏。
+     * 设计类时通常将实例变量使用private修饰，意味着该字段对其他类隐藏，对外提供public get set方法维护字段值
      * 封装好处
      * 类使用者无法触及类中隐藏部分
      * 类开发者对类中隐藏部分进行修改不会影响类的使用者
